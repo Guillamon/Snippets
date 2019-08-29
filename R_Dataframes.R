42e581aca7153f2c4d1e273fcbd77b551561c63e
Guillamon <45298036+Guillamon@users.noreply.github.com>
2019-08-29 11:06:28 +0200

    Update R_Dataframes.R

--- R_Dataframes.R
+++ R_Dataframes.R
@@ -3,3 +3,9 @@
 df %>% 
   group_by(ITEM) %>% 
   summarise_at(vars(QUANTITY),sum)
+
+
+# CONVERT LIST OF DATAFRAMES TO SINGLE DATAFRAME -binds by row
+# Use literally .id = "column_label"
+
+bind_rows(list(df1,df2), .id = "column_label")
